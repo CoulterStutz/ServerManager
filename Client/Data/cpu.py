d2216f2b95b01f9f212db8c3de5ab788486be4f5
coulterminer <coultercash@gmail.com>
2024-02-18 19:46:54 -0700

    [CLIENT]: Return and Optimize Fetching for CPU

--- Client/Data/cpu.py
+++ Client/Data/cpu.py
@@ -2,12 +2,9 @@ import psutil
 
 class CPU():
     def __init__(self):
-        self.core_count = None
+        self.core_count = self.core_count = psutil.cpu_count(logical=False)
         self.cpu_usage = None
         self.core_usage = None
-
-    def fetch_core_count(self):
-        self.core_count = psutil.cpu_count(logical=False)
 
     def fetch_cpu_usage(self):
         self.cpu_usage = psutil.cpu_percent(interval=1)
@@ -15,12 +12,13 @@ class CPU():
     def fetch_core_usage(self):
         self.core_usage = psutil.cpu_percent(interval=1, percpu=True)
 
+    def return_data(self):
+        self.fetch_core_usage()
+        self.fetch_cpu_usage()
+
+        return {"CoreCount": self.core_count, "CPU Usage": self.cpu_usage, "CoreUsage": self.core_usage}
+
 # Example usage
 if __name__ == '__main__':
-    cpu = CPU()
-    cpu.fetch_core_count()
-    cpu.fetch_cpu_usage()
-    cpu.fetch_core_usage()
-    print(cpu.core_count)
-    print(cpu.cpu_usage)
-    print(cpu.core_usage)
+    c = CPU()
+    print(c.return_data())
