64110fbfb4254e239f8cdf845f4050d55b31dda3
coulterminer <coultercash@gmail.com>
2024-02-18 19:37:19 -0700

    [CLIENT]: CPU CORE MODULE

---
+++ Client/Data/cpu.py
@@ -0,0 +1,12 @@
+class CPU():
+    def __init__(self):
+        self.cpu_name = None;
+        self.core_count = None;
+        self.cpu_usage = None;
+        self.core_usage = None
+        self.cpu_temp = None;
+        self.core_temp = None;
+
+        def fetch_cpu_data():
+            # Put Code Here
+            None
