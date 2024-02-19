c3bb22d2731a5642c64ff884612ee21f947c6a0a
coulterminer <coultercash@gmail.com>
2024-02-18 20:51:31 -0700

    FINISH CLIENT

--- Client/main.py
+++ Client/main.py
@@ -1,7 +1,14 @@
-import sys
+import sys, time
 sys.path.append("/Data")
 import Data
+import webhook
+import threading
 
-d = Data.DataManager()
+wh = webhook.WebhookManager()
+thread = threading.Thread(target=wh.run())
+thread.start()
+dm = Data.DataManager()
 
-print(d.return_data_json())
+while True:
+    wh.update_data(dm.return_data_json())
+    time.sleep(1)
---
+++ Client/webhook.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, request
+import sys, time
+sys.path.append("/Data")
+import Data
+
+class WebhookManager():
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.dm = Data.DataManager()
+        self.data = {"message": "Initial message"}
+
+        @self.app.route('/webhook')
+        def webhook():
+            self.update_data()
+            return self.data
+
+    def update_data(self):
+        self.data = self.dm.return_data_json()
+    def run(self):
+        self.app.run(port=5000)
