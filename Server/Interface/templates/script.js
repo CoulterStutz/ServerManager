e1c65182eaa515cf90f41cea1deea290ca25b529
Coulter <coultercash@gmail.com>
2024-02-20 10:24:17 -0700

    [DB]: Start Integration

---
+++ Server/Interface/templates/script.js
@@ -0,0 +1,11 @@
+let db;
+fetch('stats.db')
+  .then(response => response.arrayBuffer())
+  .then(buffer => {
+    db = new SQL.Database(new Uint8Array(buffer));
+    const data = db.exec('SELECT * FROM Registry');
+    console.log('Data:', data);
+  })
+  .catch(error => {
+    console.error('Error opening database:', error);
+  });
