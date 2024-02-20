68427257120ccdced1e4e0b23381eda09d2cdd14
Coulter <coultercash@gmail.com>
2024-02-20 08:26:17 -0700

    [SERVER]: SET UP AUTO FETCHING

--- Server/main.py
+++ Server/main.py
@@ -1,2 +1,10 @@
-import requests, json
+import requests, json, time, webhook
 
+# Implement json config
+servers = {"TestServer1":"http://127.0.0.1:5000/webhook"}
+server_stats = {"TestServer1": None}
+
+while True:
+    server_stats = webhook.fetch_server_data(servers)
+    print(server_stats)
+    time.sleep(1)
