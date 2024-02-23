8b5eb7ee7ee63c8b7c75bab2e4d3ccb389dc4d8f
coulterminer <coultercash@gmail.com>
2024-02-22 17:37:44 -0700

    [METRICS]: Implement Metrics into System

--- Server/main.py
+++ Server/main.py
@@ -1,13 +1,15 @@
 import requests, json, time, webhook
-import sqlite3
+import sqlite3, AWS
 
 # Implement json config
 servers = {"TestServer":"http://127.0.0.1:5000/webhook"}
 server_stats = {"TestServer": None}
 
+cw = AWS.CloudWatch("us-west-2")
+
 while True:
     server_stats = webhook.fetch_server_data(servers)
     for x in server_stats:
         print(server_stats[x])
-        db.write_data(x, server_stats[x])
+        cw.report_metric(x, server_stats[x])
     time.sleep(1)
