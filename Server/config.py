fb93b0345b16615eed1f52743d981f6931a814ef
coulterminer <coultercash@gmail.com>
2024-02-22 20:27:19 -0700

    UPDATE CONFIG

--- Server/config.py
+++ Server/config.py
@@ -1,5 +1,5 @@
 QuerySettings = {
-    "FetchAndReportDelay": 10,
+    "FetchAndReportDelay": 60,
     "minimalistMode": False,
     "QueryServers": {
         "TestServer": "http://127.0.0.1:5000/webhook"
