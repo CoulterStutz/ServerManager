744f5b442b76055f3d0d106fb97c2a7a78392764
coulterminer <coultercash@gmail.com>
2024-02-22 17:17:16 -0700

    [REMOVE DB]

--- Server/main.py
+++ Server/main.py
@@ -1,10 +1,9 @@
-import requests, json, time, webhook, Database
+import requests, json, time, webhook
 import sqlite3
 
 # Implement json config
 servers = {"TestServer":"http://127.0.0.1:5000/webhook"}
 server_stats = {"TestServer": None}
-db = Database.RegistryDB("Registry.db")
 
 while True:
     server_stats = webhook.fetch_server_data(servers)
