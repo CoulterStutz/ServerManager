3edb6056b5f78ab1f0ec94353cb3619d9cadb2e7
coulterminer <coultercash@gmail.com>
2024-02-24 17:37:25 -0700

    [INTERFACE]: IMPLEMENT INTERFACE TO __INIT__

--- Server/interface/__init__.py
+++ Server/interface/__init__.py
@@ -3,15 +3,15 @@ from flask import *
 class Interface():
     def __init__(self, InterfaceConfig):
         self.InterfaceConfig = InterfaceConfig
+        self.interface = Flask("__name__")
 
         self.ip = InterfaceConfig["IP"]
         self.port = InterfaceConfig["Port"]
         self.passwordAuthentication = InterfaceConfig["PasswordAuthentication"]
-        self.metrics = InterfaceConfig["Metrics"]
 
+        self.metrics = InterfaceConfig["Metrics"]
         self.CPUMetrics = self.metrics["AWSCPUMetricsLink"]
         self.CPUMetrics = self.metrics["AWSStorageMetricsLink"]
         self.CPUMetrics = self.metrics["AWSNetworkMetricsLink"]
         self.CPUMetrics = self.metrics["AWSRamMetricsLink"]
-
         self.custom_metrics = self.metrics["CustomMetrics"]
