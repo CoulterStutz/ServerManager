1f1078903f052aeeb0c791d13e150b72ce9f1e93
coulterminer <coultercash@gmail.com>
2024-02-27 17:57:52 -0700

    [RAM]: Make Ram Data Capturing Process Work on Linux

--- Client/Data/mem.py
+++ Client/Data/mem.py
@@ -1,50 +1,23 @@
 import psutil
-import wmi
 
 class RAM():
     def __init__(self):
-        self.total_ram_sticks = None
         self.total_ram_gb = None
-        self.ram_speeds = None
         self.ram_usage = None
-        self.stick_usage = None
 
-        # Fetch total RAM sticks and total RAM in gigabytes
-        w = wmi.WMI()
-        ram_modules = w.Win32_PhysicalMemory()
-        self.total_ram_sticks = len(ram_modules)
+        # Fetch total RAM in gigabytes
         self.total_ram_gb = psutil.virtual_memory().total / (1024 ** 3)  # Convert bytes to gigabytes
 
     def fetch_ram_data(self):
-        # Use WMI to get detailed RAM information including RAM speed of each stick
-        try:
-            w = wmi.WMI()
-            ram_modules = w.Win32_PhysicalMemory()
-            self.ram_speeds = [int(module.ConfiguredClockSpeed) for module in ram_modules]
-
-            # Calculate stick usage using Capacity and FreePhysicalMemory attributes
-            os_info = w.Win32_OperatingSystem()[0]
-            total_physical_memory = int(os_info.TotalVisibleMemorySize)
-            free_physical_memory = int(os_info.FreePhysicalMemory)
-            self.stick_usage = [int(module.Capacity) - free_physical_memory * 1024 for module in ram_modules]
-
-            self.ram_usage = (total_physical_memory - free_physical_memory) / total_physical_memory * 100
-        except:
-            self.total_ram_sticks = 0
-            self.total_ram_gb = 0
-            self.total_ram_speeds = 0
-            self.ram_usage = 0
-            self.stick_usage = 0
+        # Use psutil to get RAM usage
+        self.ram_usage = psutil.virtual_memory().percent
 
     def return_ram_data(self):
         self.fetch_ram_data()
 
         return {
-            "TotalRAMSticks": self.total_ram_sticks,
             "TotalRAMGB": self.total_ram_gb,
-            "RAMSpeeds": self.ram_speeds,
-            "RAMUsage": self.ram_usage,
-            "StickUsage": self.stick_usage
+            "RAMUsage": self.ram_usage
         }
 
 # Example usage
