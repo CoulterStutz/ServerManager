6600f3dcc03a2126d8400423a2100b26b4a161e2
Coulter <coultercash@gmail.com>
2024-03-05 09:14:27 -0700

    [IMPLEMENTATION]: Implement shutdown authentication

--- Client/webhook.py
+++ Client/webhook.py
@@ -1,5 +1,10 @@
+import os
+
 from flask import Flask, jsonify, request
 import sys, time
+
+from Client.config import AuthenticationSettings
+
 sys.path.append("/Data")
 import Data
 
@@ -14,9 +19,23 @@ class WebhookManager():
             self.update_data()
             return self.data
 
-        @self.app.route('/shutdown')
+        @self.app.route('/shutdown', methods=['POST'])
         def shutdown():
-            None
+            auth_header = request.headers.get('Authorization')
+            if not auth_header or 'Basic ' not in auth_header:
+                return jsonify({"error": "Unauthorized"}), 401
+
+            auth_token = auth_header.split(' ')[1]
+            username, password = auth_token.split(':')
+
+            if username not in AuthenticationSettings['Users'] or AuthenticationSettings['Users'][username]['password'] != password:
+                return jsonify({"error": "Unauthorized"}), 401
+
+            if not AuthenticationSettings['Users'][username]['Permissions']['Shutdown']:
+                return jsonify({"error": "Permission denied"}), 403
+
+            os.system('shutdown /s /t 1')
+            return jsonify({"message": "Shutting down"}), 200
 
     def update_data(self):
         self.data = self.dm.return_data_json()
