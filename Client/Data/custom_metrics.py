e7feb5070df1bd418872ac5548ba12f6256d5e1e
CoulterStutz <coultercash@gmail.com>
2024-03-05 16:45:48 -0800

    Update custom_metrics.py

--- Client/Data/custom_metrics.py
+++ Client/Data/custom_metrics.py
@@ -1,4 +1,11 @@
 class CustomMetric():
-    def __init__(namespace):
-       self.namespace = namespace
-       self.custom_metrics = {}
+    def __init__(self, name, value):
+       self.name = name
+       self.value = value
+
+    def put_value(value):
+        self.value = value
+
+class CustomMetrics():
+    def __init__(self):
+        self.custom_metrics = []
