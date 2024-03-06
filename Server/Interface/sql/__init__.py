6cac55c7ed63644ca6ff83ff4e47ac39233e069f
CoulterStutz <coultercash@gmail.com>
2024-03-05 17:42:26 -0800

    [INTERFACESQL]: Populate __init__.py

---
+++ Server/Interface/sql/__init__.py
@@ -0,0 +1,14 @@
+from .check_auth_db import check_auth
+from .check_permissions_db import check_permission
+from .create_auth_db import create_auth_database
+from .create_permissions_db import create_permissions_database
+from .insert_auth_db import insert_into_auth
+from .insert_permissions_db import insert_into_permissions
+
+class AuthDB():
+    def __init__(self):
+        None
+
+class PermissionsDB():
+    def __init__(self):
+        None
