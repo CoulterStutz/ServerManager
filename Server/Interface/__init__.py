1a9fb718d7464bcd4692954191005d257e1736df
CoulterStutz <coultercash@gmail.com>
2024-03-05 17:18:46 -0800

    [INTERFACE]: DEFINE SOCKET INTERFACE

--- Server/Interface/__init__.py
+++ Server/Interface/__init__.py
@@ -1,1 +1,7 @@
-gi
+import socket, threading
+
+class InterfaceServer():
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
