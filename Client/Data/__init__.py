8206d92c33457ca08cabdaf14d143983a1362371
Coulter <coultercash@gmail.com>
2024-03-08 09:12:11 -0700

    [DATA]: Put temps, gpu in module

--- Client/Data/__init__.py
+++ Client/Data/__init__.py
@@ -4,6 +4,8 @@ from .cpu import CPU
 from .storage import Storage
 from .network import Network
 from .mem import RAM
+from .gpu import GPU
+from .temps import SystemTemperatures
 import json
 
 class DataManager():
@@ -12,18 +14,24 @@ class DataManager():
         self.storage_manager = Storage()
         self.network_manager = Network()
         self.ram_manager = RAM()
+        self.gpu_manager = GPU()
+        self.temps_manager = SystemTemperatures()
 
     def get_all_data(self):
         cpu_data = self.cpu_manager.return_cpu_data()
         storage_data = self.storage_manager.return_storage_data()
         network_data = self.network_manager.return_network_data()
         ram_data = self.ram_manager.return_ram_data()
+        gpu_data = self.gpu_manager.return_gpu_data()
+        temps_data = self.temps_manager.get_storage_temp()
 
         data = {
             "CPUData": cpu_data,
             "StorageData": storage_data,
             "NetworkData": network_data,
-            "RAMData": ram_data
+            "RAMData": ram_data,
+            "GPUData": gpu_data,
+            "Temps": temps_data
         }
 
         return data
